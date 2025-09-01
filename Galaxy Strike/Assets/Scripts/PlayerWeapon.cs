8299ea5467ad755e31025e032d94503ae544788b
itsChisomm <chisomc@hotmail.co.uk>
2025-09-01 13:05:35 +0100

    Adding Target Aim

--- Galaxy Strike/Assets/Scripts/PlayerWeapon.cs
+++ Galaxy Strike/Assets/Scripts/PlayerWeapon.cs
@@ -5,6 +5,8 @@ public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField] GameObject[] lasers;
     [SerializeField] RectTransform crosshair;
+    [SerializeField] Transform targetPoint;
+    [SerializeField] float targetDistance = 100f;
 
     bool isFiring = false;
 
@@ -17,6 +19,8 @@ public class PlayerWeapon : MonoBehaviour
     {
         ProcessFiring();
         MoveCrosshair();
+        MoveTargetPoint();
+        AimLasers();
     }
     public void OnFire(InputValue value)
     {
@@ -37,6 +41,23 @@ public class PlayerWeapon : MonoBehaviour
     void MoveCrosshair()
     {
                
-        crosshair.position = Mouse.current.position.ReadValue(); ;
+        crosshair.position = Mouse.current.position.ReadValue(); 
+    }
+
+    void MoveTargetPoint()
+    {
+
+        Vector3 targetPointPosition = new Vector3(crosshair.position.x , crosshair.position.y, targetDistance);
+        targetPoint.position = Camera.main.ScreenToWorldPoint(targetPointPosition);
+    }
+
+    void AimLasers()
+    {
+               foreach (GameObject laser in lasers)
+        {
+            Vector3 fireDirection = targetPoint.position - laser.transform.position; // Calculate the direction from the laser to the target point
+            Quaternion rotationToTarget = Quaternion.LookRotation(fireDirection); // Calculate the rotation needed to look at the target point
+            laser.transform.rotation = rotationToTarget; // Aim the laser at the target point
+        }
     }
 }
