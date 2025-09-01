2dff4dc132ca2e396c7141c81445553d822efa52
itsChisomm <chisomc@hotmail.co.uk>
2025-09-01 19:45:03 +0100

    Adding Music & Sound effects

--- Galaxy Strike/Assets/Scripts/CollisionHandler.cs
+++ Galaxy Strike/Assets/Scripts/CollisionHandler.cs
@@ -5,8 +5,16 @@ public class CollisionHandler : MonoBehaviour
 
     [SerializeField] GameObject destroyedVFX;
 
+    GameSceneManager gameSceneManager;
+
+    void Start()
+    {
+        gameSceneManager = FindFirstObjectByType<GameSceneManager>();
+    }
+
     void OnTriggerEnter(Collider other)
    {
+        gameSceneManager.ReloadLevel();
         Instantiate(destroyedVFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
