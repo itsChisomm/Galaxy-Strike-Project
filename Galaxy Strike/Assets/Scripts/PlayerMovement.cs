b314b5b2e3cb939f7d805d8fb9e7c776184621fc
itsChisomm <chisomc@hotmail.co.uk>
2025-09-01 10:32:44 +0100

    Setting up Input Action Movement

--- Galaxy Strike/Assets/Scripts/PlayerMovement.cs
+++ Galaxy Strike/Assets/Scripts/PlayerMovement.cs
@@ -3,20 +3,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] float controlSpeed = 10f;
+
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessTranslation();
     }
 
     public void OnMove(InputValue value)
     {
-       Debug.Log(value.Get<Vector2>());
+       movement = value.Get<Vector2>();
+    }
+
+    private void ProcessTranslation()
+    {
+        float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float yOffset = movement.y * controlSpeed * Time.deltaTime;
+
+        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
     }
 }
