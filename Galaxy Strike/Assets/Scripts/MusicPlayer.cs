53cdc9e987db8cb030398ef06202655188533884
itsChisomm <chisomc@hotmail.co.uk>
2025-09-01 20:54:41 +0100

    Completing Level
    Vertical Slice

--- Galaxy Strike/Assets/Scripts/MusicPlayer.cs
+++ Galaxy Strike/Assets/Scripts/MusicPlayer.cs
@@ -5,7 +5,16 @@ public class MusicPlayer : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int numOfMusicPlayers = FindObjectsByType<MusicPlayer>(FindObjectsSortMode.None).Length; // Count the number of MusicPlayer instances in the scene
+
+        if (numOfMusicPlayers > 1) // If there is more than one MusicPlayer
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
     // Update is called once per frame
